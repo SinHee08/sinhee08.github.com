71b7b7cc321334e97b87a8967d4a5756e535b1d7
tansinhee <105835401+tansinhee@users.noreply.github.com>
2024-01-23 17:43:12 +0800

    create js
    javascript

---
+++ JavascriptPratise/script.js
@@ -0,0 +1,53 @@
+// document.write("<h1>信熙好帅</h1>");
+
+/*====================基本资料形态 & 变数======================*/
+// 打字
+"hello world"
+
+// 数字
+123
+
+// 布林值
+true
+false
+
+// 只能是英文大小写或是数字或是$或是_的组合
+// 开头不能是数字
+// var my_name = "isaac";
+// var my_age = 24;
+// var is_male = true;
+ 
+// document.write("有一个人叫 " + my_name);
+// document.write("<br/>");
+// document.write(my_name + " is " + my_age);
+// document.write("<br/>");
+// document.write(my_name +" is 5ft7 tall");
+// document.write("<br/>");
+// document.write(my_name +" hate to be 24");
+
+/*====================制作一个基本计算机======================*/
+//prompt默认打字而已
+// var num_1 = prompt("pls insert num 1");
+// var num_2 = prompt("pls insert num 2");
+
+//parseInt可以把string改去正数
+// num_1 = parseInt(num_1)
+// num_2 = parseInt(num_2)
+
+//parseFloat可以把string改去小数点
+// num_1 = parseFloat(num_1)
+// num_2 = parseFloat(num_2)
+// document.write(num_1 + num_2);.
+
+/*====================阵列 array======================*/
+var scores = [80,60,20,30,50];
+var friends = ["Ben","Alson","Dickson"]
+scores[0] = 20;
+var score1 = 80;
+var score2 = 60;
+var score3 = 20;
+var score4 = 30;
+var score5 = 50;
+document.write(scores[0]);
+document.write(friends[0]);
+document.write(friends.length);
